451a1ea648236ad56e9472ca3c5c5c66d21d38e7
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-05-31 15:30:30 +0300

    31.05.2022 - max and min brightness keys

--- bash-scripts/adjust-brightness.sh
+++ bash-scripts/adjust-brightness.sh
@@ -2,13 +2,24 @@
 set -e
 file="/sys/class/backlight/amdgpu_bl0/brightness"
 current=$(cat "$file")
+max_brightness=255
+min_brightness=0
 new="$current"
-if [ "$1" = "-inc" ]
+if [ "$1" = "--inc" ]
 then
     new=$(( current + $2 ))
 fi
-if [ "$1" = "-dec" ]
+if [ "$1" = "--dec" ]
 then
     new=$(( current - $2 ))
 fi
+if [ "$1" = "--min" ]
+then
+    new=$min_brightness
+fi
+if [ "$1" = "--max" ]
+then
+    new=$max_brightness
+fi
+
 echo "$new" | tee "$file"
