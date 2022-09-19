c67d0acae666429284897388cc1b8eeec3364bad
Toma Adrian-Ionut <sirhadrian21@gmail.com>
2022-09-19 18:39:14 +0300

    update auto-ranger-install

--- ranger/auto-ranger-setup.sh
+++ ranger/auto-ranger-setup.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
 
-CONFIGDIR=$HOME'/.config/ranger'
+install_ranger() {
+    CONFIGDIR=$HOME'/.config/ranger'
 
-echo $CONFIGDIR
+    [[ ! -d $CONFIGDIR ]] && mkdir $CONFIGDIR
 
-[[ ! -d $CONFIGDIR ]] && mkdir $CONFIGDIR
+    cp rc.conf rifle.conf scope.sh $CONFIGDIR
 
-cp rc.conf rifle.conf scope.sh $CONFIGDIR
+    if [[ $(which pacman) ]]; then
+        # Refresh pkg database
+        sudo pacman -Syy
+        # Check if ranger is installed
+        [[ ! $(which ranger) ]] && sudo pacman -S --needed --noconfirm ranger
 
-if [[ $(which pacman) ]]; then
-    # Refresh pkg database
-    sudo pacman -Syy
-    # Check if ranger is installed
-    [[ ! $(which ranger) ]] && sudo pacman -S --needed --noconfirm ranger
-    
-    # Images thumbnails
-    # For pictures preview
-    sudo pacman -S --needed --noconfirm ueberzug
+        # Images thumbnails
+        # For pictures preview
+        sudo pacman -S --needed --noconfirm ueberzug
 
-    # For epub preview
-    # Check if pip is installed
-    if [[ ! $(which pip) ]]; then
-        sudo pacman -S --needed --noconfirm python-pip
+        # For epub preview
+        # Check if pip is installed
+        [[ ! $(which pip) ]] && sudo pacman -S --needed --noconfirm python-pip
+
+        # Install Pillow dependecy if not installed'
+        [[ $(python -c "import PIL") ]] && pip install Pillow
+
+        # Build epub-thumbnailer from source and clean
+        git clone https://github.com/marianosimone/epub-thumbnailer.git && sudo python epub-thumbnailer/install.py install && rm -rf epub-thumbnailer
+
+        # Video thumbnailer
+        sudo pacman -S --needed --noconfirm ffmpeg ffmpegthumbnailer
+
+    elif [[ $(which dnf) ]]; then
+        echo "TODO FEDORA"
+    else
+        exit 1
     fi
-    # Install Pillow dependecy if not installed'
-    if [[ $(python -c "import PIL") ]]; then
-        pip install Pillow
-    fi
-    # Build epub-thumbnailer from source and clean
-    git clone https://github.com/marianosimone/epub-thumbnailer.git && sudo python epub-thumbnailer/install.py install && rm -rf epub-thumbnailer
+}
 
-    # Video thumbnailer
-    sudo pacman -S --needed --noconfirm ffmpeg ffmpegthumbnailer
+# Ask for confirmation
+defaultValue=Y
+read -p "Do you want to install ranger? (Y/n)" answer
 
-elif [[ $(which dnf) ]]; then
-    echo "TODO FEDORA"
-else
+doInstall=${answer:-$defaultValue}
+
+case $doInstall in
+[Yy]) install_ranger ;;
+[Nn]) exit 0 ;;
+*)
+    echo "Incorrect option"
     exit 1
-fi
+    ;;
+esac
