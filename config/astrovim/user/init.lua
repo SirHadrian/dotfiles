fa6dd4346d990e0d4e4cd050dda2bcc67152be6f
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-11-18 14:45:58 +0200

    vimscript zoom in and out in gui mode

--- config/astrovim/user/init.lua
+++ config/astrovim/user/init.lua
@@ -338,7 +338,16 @@ vim.cmd([[
   
     " Neovide settings
     if exists("g:neovide")
-        set guifont=JetBrainsMono\ Nerd\ Font:h6
+
+      set guifont=JetBrainsMono\ Nerd\ Font:h8
+        
+      let g:neovide_scale_factor=1.0
+      function! ChangeScaleFactor(delta)
+          let g:neovide_scale_factor = g:neovide_scale_factor * a:delta
+      endfunction
+      nnoremap <expr><C-=> ChangeScaleFactor(1.25)
+      nnoremap <expr><C--> ChangeScaleFactor(1/1.25)
+
     endif
 
 ]])
