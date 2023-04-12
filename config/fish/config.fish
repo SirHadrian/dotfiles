648c7d4105dbaf94517cc170877ecb2c4456699f
Toma Adrian Ionut <sirhadrian21@gmail.com>
2023-04-12 14:43:19 +0300

    extract function and HDMI alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,12 +7,17 @@ if status is-interactive
     
     # Git
     alias gp 'git push'
-    alias gcom 'git add . ; git commit'
+    alias gcom 'git add . && git commit'
     alias gst 'git status'
     alias gcl 'git clone'
+    alias glog 'git log --oneline --decorate --graph'
 
     # X11 settings
     alias noblackscreen 'xset s off; xset -dpms; xset s noblank'
+
+    # HDMI monitor
+    alias start-HDMI 'xrandr --output HDMI-1-0 --mode 1366x768 --auto --right-of eDP-1 && sleep 2 && leftwm command SoftReload'
+    alias stop-HDMI 'xrandr --output HDMI-1-0 --off && sleep 2 && leftwm command SoftReload'
 
     # Terminal
     alias .. 'cd ..'
@@ -38,8 +43,47 @@ if status is-interactive
     mv "$argv" ~/Trash
    end 
 
+   # Run job silently
+   function p
+       $argv &> /dev/null &
+   end
+   
+   function makedir
+       mkdir -p "$argv" && cd "$argv"
+   end
+
+   function extract
+       if test -f "$argv"
+           switch "$argv"
+               case "*.tar.gz"
+                   tar -xzvf "$argv"
+               case "*.bz2"
+                   bunzip2 "$argv"
+               case "*.rar"
+                   unrar x "$argv"
+               case "*.tar"
+                   tar -xvf "$argv"
+               case "*.tbz2"
+                   tar -xjvf "$argv"
+               case "*.tgz" 
+                   tar -xzvf "$argv"
+               case "*.zip"
+                   unzip "$argv"
+               case "*.Z"
+                   uncompress "$argv"
+               case "*.7z"
+                   7z x "$argv"
+               case "*"
+                   printf "%s" "Unrecognized file extension"
+           end
+       else
+           printf "%s" "The file does not exist"
+       end
+   end
+
    # Key binds
    bind \cp 'cmus'
+   bind \ch 'htop'
    
    # Update PATH
    fish_add_path ~/.cargo/bin
