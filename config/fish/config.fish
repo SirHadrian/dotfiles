0696654964001eb89be5db73edaced07ee86af68
Toma Adrian <sirhadrian21@gmail.com>
2023-05-11 21:52:41 +0300

    functions descriptions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     # set fish_greeting
-
+    
     set fish_greeting
     
-    # Aliases
+    set -gx EDITOR nvim
+    
+    # ========================================================================================
+    
+    # ALIASES
     # alias automatically appends $argv, so that all parameters used with the alias are passed to the actual command.
     
     # Git
@@ -13,14 +17,14 @@ if status is-interactive
     alias gst 'git status'
     alias gcl 'git clone'
     alias glog 'git log --oneline --decorate --graph'
-
+    
     # X11 settings
     alias noblackscreen 'xset s off; xset -dpms; xset s noblank'
-
+    
     # HDMI monitor
     alias start-HDMI 'xrandr --output HDMI-1-0 --mode 1366x768 --auto --right-of eDP-1 && sleep 2 && leftwm command SoftReload'
     alias stop-HDMI 'xrandr --output HDMI-1-0 --off && sleep 2 && leftwm command SoftReload'
-
+    
     # Terminal
     alias .. 'cd ..'
     alias ... 'cd ../..'
@@ -31,72 +35,87 @@ if status is-interactive
     alias ra 'ranger'
     alias cat 'bat'
     alias f 'xdg-open (fzf) &> /dev/null'
-
+    
     # Systemctl
     alias sc 'sudo systemctl'
-
-   # Neovide
-   alias nv 'neovide --multigrid'
-
-   # Nvidia optimus
-   function prime-run
-       __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only $argv &> /dev/null &
-   end
-
-   # Move file to trash
-   function t
-    if ! test -d ~/Trash
-        mkdir ~/Trash
+    
+    # Start ssh agent with key
+    alias gu 'ssh-add ~/.ssh/id_ed25519 && gitui'
+    
+    # Neovide
+    alias nv 'neovide --multigrid'
+    
+    # ========================================================================================
+    
+    # FUNCTIONS
+    
+    function prime-run --description "Run program using the nvidia GPU"
+        __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only $argv &> /dev/null &
     end
-    mv "$argv" ~/Trash
-   end 
+    
+    function t --description "Send file to trash"
+     if ! test -d ~/Trash
+         mkdir ~/Trash
+     end
+     mv "$argv" ~/Trash
+    end 
+    
+    function p --description "Run a program in the background silently"
+        $argv &> /dev/null &
+    end
+    
+    function o --description "Open a file with the default application"
+        xdg-open "$argv" &> /dev/null &
+    end
+    
+    function makedir --description "Make a new directory and cd into it"
+        mkdir -p "$argv" && cd "$argv"
+    end
+    
+    function extract --description "Extract archives"
+        if test -f "$argv"
+            switch "$argv"
+                case "*.tar.gz"
+                    tar -xzvf "$argv"
+                case "*.bz2"
+                    bunzip2 "$argv"
+                case "*.rar"
+                    unrar x "$argv"
+                case "*.tar"
+                    tar -xvf "$argv"
+                case "*.tbz2"
+                    tar -xjvf "$argv"
+                case "*.tgz" 
+                    tar -xzvf "$argv"
+                case "*.zip"
+                    unzip "$argv"
+                case "*.Z"
+                    uncompress "$argv"
+                case "*.7z"
+                    7z x "$argv"
+                case "*"
+                    printf "%s" "Unrecognized file extension"
+            end
+        else
+            printf "%s" "The file does not exist"
+        end
+    end
+    
+    # ========================================================================================
+    
+    # KEYBINDINGS
+    
+    bind \cp 'cmus'
+    bind \ch 'htop'
+    bind \cf 'xdg-open (fzf) &> /dev/null'  # file search
+    bind \ee 'nvim'
+    
+    # ========================================================================================
+    
+    # Update PATH
+    fish_add_path ~/.cargo/bin
+    
+    # Init starship prompt (must be last)
+    starship init fish | source
 
-   # Run job silently
-   function p
-       $argv &> /dev/null &
-   end
-   
-   function makedir
-       mkdir -p "$argv" && cd "$argv"
-   end
-
-   function extract
-       if test -f "$argv"
-           switch "$argv"
-               case "*.tar.gz"
-                   tar -xzvf "$argv"
-               case "*.bz2"
-                   bunzip2 "$argv"
-               case "*.rar"
-                   unrar x "$argv"
-               case "*.tar"
-                   tar -xvf "$argv"
-               case "*.tbz2"
-                   tar -xjvf "$argv"
-               case "*.tgz" 
-                   tar -xzvf "$argv"
-               case "*.zip"
-                   unzip "$argv"
-               case "*.Z"
-                   uncompress "$argv"
-               case "*.7z"
-                   7z x "$argv"
-               case "*"
-                   printf "%s" "Unrecognized file extension"
-           end
-       else
-           printf "%s" "The file does not exist"
-       end
-   end
-
-   # Key binds
-   bind \cp 'cmus'
-   bind \ch 'htop'
-   bind \cf 'xdg-open (fzf) &> /dev/null'  # file search
-
-   # Update PATH
-   fish_add_path ~/.cargo/bin
-
-   # Init starship prompt (must be last)
-   starship init fish | source
 end
