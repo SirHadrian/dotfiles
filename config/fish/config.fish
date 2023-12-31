3f1cf403bc895fde87f79bf3e6bb5e3b0c7ebdcb
Toma Adrian Ionut <sirhadrian21@gmail.com>
2023-12-31 23:43:24 +0200

    updated dotfiles to the latest versions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,146 +1,187 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # set fish_greeting
-    
-    set fish_greeting
-    
-    set -gx EDITOR nvim
+  # Commands to run in interactive sessions can go here
+  # set fish_greeting
 
-    # Vi mode
-    fish_vi_key_bindings
-    # Emacs mode
-    #fish_default_key_bindings
-    
-    # ========================================================================================
-    
-    # ALIASES
-    # alias automatically appends $argv, so that all parameters used with the alias are passed to the actual command.
-    
-    # Git
-    alias gp 'git push'
-    alias gcom 'git add . && git commit'
-    alias gst 'git status'
-    alias gcl 'git clone'
-    alias glog 'git log --oneline --decorate --graph'
-    
-    # X11 settings
-    alias noblackscreen 'xset s off; xset -dpms; xset s noblank'
-    
-    # HDMI monitor
-    alias start-HDMI 'xrandr --output HDMI-1-0 --mode 1366x768 --auto --right-of eDP-1 && sleep 2 && leftwm command SoftReload'
-    alias stop-HDMI 'xrandr --output HDMI-1-0 --off && sleep 2 && leftwm command SoftReload'
-    
-    # Terminal
-    alias .. 'cd ..'
-    alias ... 'cd ../..'
-    alias ls 'exa'
-    alias l 'exa -l'
-    alias la 'exa -la'
-    alias lr 'exa -lR'
-    alias ra 'ranger'
-    alias cat 'bat'
-    alias ti 'date +"%H : %M" | figlet -f standard | lolcat -r'
-    
-    # Systemctl
-    alias sc 'sudo systemctl'
-    
-    # Start ssh agent with key
-    alias gu 'ssh-add ~/.ssh/id_ed25519 && gitui'
-    
-    # Neovide
-    alias nv 'neovide --multigrid'
-    # Neovim
-    alias v 'nvim'
+  set fish_greeting
 
-    # Animated background
-    alias animvid-gpu 'prime-run xwinwrap -g 1920x1080 -ov -ni -s -nf -un -fs -b -- mpv --hwdec=vdpau --vo=gpu -wid WID --loop --no-border --no-config --no-window-dragging --no-input-default-bindings --no-osd-bar --no-sub --no-audio'
-    
-    # ========================================================================================
-    
-    # FUNCTIONS
-    
-    function prime-run --description "Run program using the nvidia GPU"
-        __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only $argv &> /dev/null &
-    end
-    
-    function t --description "Send file to trash"
-     if ! test -d ~/Trash
-         mkdir ~/Trash
-     end
-     mv $argv ~/Trash
-    end 
-    
-    function p --description "Run a program in the background silently"
-        $argv &> /dev/null &
-    end
-    
-    function o --description "Open a file with the default application"
-        xdg-open "$argv" &> /dev/null &
-    end
-    
-    function makedir --description "Make a new directory and cd into it"
-        mkdir -p "$argv" && cd "$argv"
-    end
-    
-    function extract --description "Extract archives"
-        if test -f "$argv"
-            switch "$argv"
-                case "*.tar.gz"
-                    tar -xzvf "$argv"
-                case "*.bz2"
-                    bunzip2 "$argv"
-                case "*.rar"
-                    unrar x "$argv"
-                case "*.tar"
-                    tar -xvf "$argv"
-                case "*.tbz2"
-                    tar -xjvf "$argv"
-                case "*.tgz" 
-                    tar -xzvf "$argv"
-                case "*.zip"
-                    unzip "$argv"
-                case "*.Z"
-                    uncompress "$argv"
-                case "*.7z"
-                    7z x "$argv"
-                case "*"
-                    printf "%s" "Unrecognized file extension"
-            end
-        else
-            printf "%s" "The file does not exist"
-        end
-    end
+  set -gx EDITOR nvim
 
-    function f --description "Search and open files"
-        xdg-open (fzf) &> /dev/null &
-    end
+  # Vi mode
+  fish_vi_key_bindings
+  # Emacs mode
+  # fish_default_key_bindings
 
-    function hi --description "Search history file for a command"
-        history | fzf | read -l result 
-        
-        if test $result
-            commandline -rb $result
-        end
+  # ========================================================================================
+
+  # ALIASES
+  # alias automatically appends $argv, so that all parameters used with the alias are passed to the actual command.
+
+  # Git
+  alias gst 'git status'
+  alias gcl 'git clone'
+  alias glog 'git log --oneline --decorate --graph'
+
+  # X11 settings
+  alias noblackscreen 'xset s off; xset -dpms; xset s noblank'
+  alias sx 'startx ~/.xinitrc'
+
+  # HDMI monitor
+  # alias start-HDMI 'xrandr --output HDMI-1-0 --mode 1366x768 --auto --right-of eDP-1 && sleep 2 && leftwm command SoftReload'
+  # alias stop-HDMI 'xrandr --output HDMI-1-0 --off && sleep 2 && leftwm command SoftReload'
+
+  # Terminal
+  alias .. 'cd ..'
+  alias ... 'cd ../..'
+  alias ls 'exa'
+  alias l 'exa -l'
+  alias la 'exa -la'
+  alias lr 'exa -lR'
+  alias r 'ranger'
+  alias cat 'bat'
+  alias ti 'date +"%H : %M" | figlet -f standard | lolcat'
+  alias j 'jobs'
+
+  # Systemctl
+  alias sc 'sudo systemctl'
+
+  # Start ssh agent with key
+  alias gu 'ssh-add ~/.ssh/id_ed25519 && gitui'
+
+  # Neovide
+  # alias nv 'neovide --multigrid'
+  alias v 'neovide --maximized'
+  alias vv 'neovide --maximized .'
+  # Neovim
+  alias nv 'nvim'
+  alias nvv 'nvim .'
+
+  # Animated background
+  alias animvid-gpu 'prime-run xwinwrap -g 1920x1080 -ov -ni -s -nf -un -fs -b -- mpv --hwdec=vdpau --vo=gpu -wid WID --loop --no-border --no-config --no-window-dragging --no-input-default-bindings --no-osd-bar --no-sub --no-audio'
+
+  # Brightness
+  alias b 'light -S' 
+
+  # Bluetoothctl
+  alias bl 'bluetoothctl'
+
+  # Lazygit
+  alias lg 'lazygit'
+
+  # ========================================================================================
+
+  # FUNCTIONS
+
+  # function prime-run --description "Run program using the nvidia GPU"
+  #     __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only $argv &> /dev/null &
+  # end
+
+  function xc --description "Pipe to clipboard"
+    xclip -selection c
+  end
+
+  function t --description "Send file to trash"
+    if ! test -d ~/Trash
+      mkdir ~/Trash
     end
-    
-    # ========================================================================================
-    
-    # KEYBINDINGS
-    
-    # bind \cp 'cmus'
-    # bind \ch 'htop'
-    # bind \ee 'nvim'
-    
-    # ========================================================================================
-    
-    date +"%H : %M" | figlet -f standard | lolcat -r
-    
-    # Update PATH
-    fish_add_path ~/.cargo/bin ~/.ghcup/bin ~/Downloads/AppImages
-    
-    # Init zoxide (cd)
-    zoxide init fish | source
-    # Init starship prompt (must be last)
-    starship init fish | source
+    mv $argv ~/Trash
+  end 
+
+  function p --description "Run a program in the background silently"
+    $argv &> /dev/null &
+  end
+
+  function o --description "Open a file with the default application"
+    xdg-open "$argv" &> /dev/null &
+  end
+
+  function makedir --description "Make a new directory and cd into it"
+    mkdir -p "$argv" && cd "$argv"
+  end
+
+  function extract --description "Extract archives"
+    if test -f "$argv"
+      switch "$argv"
+        case "*.tar.gz"
+          tar -xzvf "$argv"
+        case "*.bz2"
+          bunzip2 "$argv"
+        case "*.rar"
+          unrar x "$argv"
+        case "*.tar"
+          tar -xvf "$argv"
+        case "*.tbz2"
+          tar -xjvf "$argv"
+        case "*.tgz" 
+          tar -xzvf "$argv"
+        case "*.zip"
+          unzip "$argv"
+        case "*.Z"
+          uncompress "$argv"
+        case "*.7z"
+          7z x "$argv"
+        case "*"
+          printf "%s" "Unrecognized file extension"
+      end
+    else
+      printf "%s" "The file does not exist"
+    end
+  end
+
+  function f --description "Search and open files"
+    xdg-open (fzf) &> /dev/null &
+  end
+
+  function hi --description "Search history file for a command"
+    history | fzf | read -l result 
+
+    if test $result
+      commandline -rb $result
+    end
+  end
+
+  function ra --description "Ranger change dir"
+
+    set tempfile '/tmp/chosendir'                                                  
+    ranger --choosedir=$tempfile (pwd)                                    
+
+    if test -f $tempfile                                                           
+      if [ (cat $tempfile) != (pwd) ]                                            
+        cd (cat $tempfile)                                                       
+      end                                                                        
+    end                                                                            
+
+    rm -f $tempfile                                                                
+
+  end
+
+  # ========================================================================================
+
+  # KEYBINDINGS
+
+  bind --mode insert \ep 'pulsemixer; commandline -f repaint'
+  bind --mode insert \em 'cmus; commandline -f repaint'
+  bind --mode insert \er 'ranger; commandline -f repaint'
+  bind --mode insert \ee 'nvim; commandline -f repaint'
+  # bind --mode insert \e1 'librewolf &>/dev/null &; commandline -f repaint'
+  bind --mode insert \ey 'flatpak run io.gitlab.librewolf-community &>/dev/null &; commandline -f repaint'
+  bind --mode insert \ew 'firefox &>/dev/null &; commandline -f repaint'
+  bind --mode insert \et 'dolphin . &>/dev/null &; commandline -f repaint'
+  bind --mode insert --sets-mode default kj 'commandline -f repaint'
+  # bind --mode insert --sets-mode default jj 'commandline -f repaint'
+  bind --mode insert --sets-mode default kk 'commandline -f repaint'
+
+  bind --mode insert \cf accept-autosuggestion
+
+  # ========================================================================================
+
+  #date +"%H : %M" | figlet -f standard | lolcat
+
+  # Update PATH
+  fish_add_path ~/.cargo/bin ~/.ghcup/bin ~/Downloads/AppImages
+
+  # Init zoxide (cd)
+  zoxide init fish | source
+  # Init starship prompt (must be last)
+  starship init fish | source
 
 end
--- config/ranger/auto-ranger-setup.sh
+++ config/ranger/auto-ranger-setup.sh
@@ -1,66 +1,70 @@
 #!/usr/bin/bash
 
 install_ranger() {
-	CONFIGDIR=$HOME'/.config/ranger'
+  CONFIGDIR=$HOME'/.config/ranger'
 
-	[[ ! -d $CONFIGDIR ]] && mkdir -p "$CONFIGDIR"
+  [[ ! -d $CONFIGDIR ]] && mkdir -p "$CONFIGDIR"
 
-	cp rc.conf rifle.conf scope.sh "$CONFIGDIR"
+  cp -rv colorschemes rc.conf rifle.conf scope.sh "$CONFIGDIR"
 
-	if [ -f /etc/os-release ]; then
-		# freedesktop.org and systemd
-		source /etc/os-release
-		OS=$ID
-	else
-		printf "%s" "Could not detect distro"
-		exit 1
-	fi
+  if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    source /etc/os-release
+    OS=$ID
+  else
+    printf "%s" "Could not detect distro"
+    exit 1
+  fi
 
-	if [[ "$OS" == "arch" ]]; then
-		# Check if ranger is installed
-		[[ -x $(command -v ranger) ]] || sudo pacman -S --needed --noconfirm ranger
+  if [[ "$OS" == "arch" ]]; then
+    # Check if ranger is installed
+    [[ -x $(command -v ranger) ]] || sudo pacman -S --needed --noconfirm ranger
 
-		# Images thumbnails
-		# For pictures preview
-		# NO MORE MANTAINED USE THE AUR PACKAGE
-		# sudo pacman -S --needed --noconfirm ueberzug
+    # Images thumbnails
+    # For pictures preview
+    # NO MORE MANTAINED USE THE AUR PACKAGE
+    # sudo pacman -S --needed --noconfirm ueberzug
 
-		# For epub preview
-		# Check if pip is installed
-		[[ -x $(command pip) ]] || sudo pacman -S --needed --noconfirm python-pip
-		# Install Pillow dependecy if not installed'
-		[[ $(python -c "import PIL") ]] && sudo pacman -S --needed --noconfirm python-pillow
+    # For epub preview
+    # Check if pip is installed
+    [[ -x $(command pip) ]] || sudo pacman -S --needed --noconfirm python-pip
+    # Install Pillow dependecy if not installed'
+    [[ $(python -c "import PIL") ]] || sudo pacman -S --needed --noconfirm python-pillow
 
-		# Video thumbnailer
-		sudo pacman -S --needed --noconfirm ffmpeg ffmpegthumbnailer
+    # Video thumbnailer
+    sudo pacman -S --needed --noconfirm ffmpeg ffmpegthumbnailer
 
-	elif [[ "$OS" == "fedora" ]]; then
-		# Check if ranger is installed
-		[[ -x $(command -v ranger) ]] || sudo dnf install -y ranger
+  elif [[ "$OS" == "fedora" ]]; then
+    # Check if ranger is installed
+    [[ -x $(command -v ranger) ]] || sudo dnf install -y ranger
 
-		# Images thumbnails
-		# For pictures preview build ueberzugpp manually
-		# https://github.com/jstkdng/ueberzugpp
+    # Images thumbnails
+    # For pictures preview build ueberzugpp manually
+    # https://github.com/jstkdng/ueberzugpp
 
-		# For epub preview
-		# Check if pip is installed
-		[[ -x $(command pip) ]] || sudo dnf install -y python-pip
+    # For epub preview
+    # Check if pip is installed
+    [[ -x $(command pip) ]] || sudo dnf install -y python-pip
 
-		# Install Pillow dependecy if not installed'
-		[[ $(python -c "import PIL") ]] && pip install Pillow
+    # Install Pillow dependecy if not installed'
+    [[ $(python -c "import PIL") ]] && pip install Pillow
 
-		# Video thumbnailer
-		sudo dnf install -y ffmpeg ffmpegthumbnailer
-	fi
+    # Video thumbnailer
+    sudo dnf install -y ffmpeg ffmpegthumbnailer
+  fi
 
-	# Build epub-thumbnailer from source and clean
-	# Install dependecy globaly with python -m pip install Pillow
-	git clone https://github.com/marianosimone/epub-thumbnailer.git && sudo python epub-thumbnailer/install.py install && rm -rf epub-thumbnailer
+  # Build epub-thumbnailer from source and clean
+  # Install dependecy globaly with python -m pip install Pillow
+  git clone https://github.com/marianosimone/epub-thumbnailer.git \
+    && sudo python epub-thumbnailer/install.py install \
+    && rm -rf epub-thumbnailer
 
-	# Ranger icons
-	git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-	echo "default_linemode devicons" >>"$CONFIGDIR"/rc.conf
+  # Ranger icons plugin
+  git clone https://github.com/alexanderjeurissen/ranger_devicons.git ~/.config/ranger/plugins/ranger_devicons
+  echo "default_linemode devicons" >>"$CONFIGDIR"/rc.conf
 
+  # Ranger zoxide plugin
+  git clone https://github.com/jchook/ranger-zoxide.git ~/.config/ranger/plugins/zoxide
 }
 
 # Ask for confirmation
@@ -69,10 +73,10 @@ read -rp "Do you want to install ranger? (Y/n)" answer
 doInstall=${answer:-Y}
 
 case $doInstall in
-[Yy]) install_ranger ;;
-[Nn]) exit 0 ;;
-*)
-	echo "Incorrect option"
-	exit 1
-	;;
+  [Yy]) install_ranger ;;
+  [Nn]) exit 0 ;;
+  *)
+    echo "Incorrect option"
+    exit 1
+    ;;
 esac
